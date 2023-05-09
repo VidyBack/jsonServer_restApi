80dcd303a74df99b672a325958bbb8b1091cf512
jayshrisonalekar <jayshrisonalekar11@gmail.com>
2023-05-09 11:34:19 +0530

    db.json updated

--- server.js
+++ server.js
@@ -18,46 +18,7 @@ server.use((req, res, next) => {
         router.db.setState(clone(data))
     next()
 })
-const { food, auto, estate, travel, autoQuote, autoEvent, autoTips, autoBlogs, sports, jobs, politics, estateTips, estateQuote } = require("./dummydata");
 
-server.use("/dummy/server/:platform", async function (req, res) {
-  try {
-    console.log(req.params.platform);
-    let collection;
-    if (req.params.platform == "food") {
-      collection = food;
-    } else if (req.params.platform == "auto") {
-      collection = auto;
-    } else if (req.params.platform == "travel") {
-      collection = travel;
-    } else if (req.params.platform == "autoQuote") {
-      collection = autoQuote;
-    } else if (req.params.platform == "autoEvent") {
-      collection = autoEvent;
-    } else if (req.params.platform == "autoTips") {
-      collection = autoTips;
-    } else if (req.params.platform == "autoBlogs") {
-      collection = autoBlogs;
-    } else if (req.params.platform == "sports") {
-      collection = sports;
-    } else if (req.params.platform == "jobs") {
-      collection = jobs;
-    } else if (req.params.platform == "politics") {
-      collection = politics;
-    } else if (req.params.platform == "estateTips") {
-      collection = estateTips;
-    } else if (req.params.platform == "estateQuote") {
-      collection = estateQuote;
-    } else {
-      collection = estate;
-    }
-    res.status(200).json(collection);
-  } catch (error) {
-    console.log(error);
-  }
-
-}
-);
 server.use(router)
 server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
